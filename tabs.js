95ac220431ed9bc20d0bc808302e894e10868ebb
Jorge Rivera <jorgeriv@gmail.com>
2015-02-28 14:03:36 -0600

    tabs script added

--- tabs.js
+++ tabs.js
@@ -2,5 +2,11 @@
 'use strict';
 
 $(function(){
-    
+    $('.tab').click(function(e){
+        var selector = '#container' + $(this).data().ref;
+        var $clicked = $(selector);
+        e.preventDefault();
+        $('.active').removeClass('active');
+        $clicked.addClass('active');
+    });
 });
